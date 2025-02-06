84f5b3c6a7fe162cf4209df7434a2a9de9554155
Luis Galvan <67127384+luis920@users.noreply.github.com>
2025-02-06 23:34:32 +0000

    fetch classes of dashboard admin to dasboard teacher

--- src/front/js/component/teacher/TeacherClasses.jsx
+++ src/front/js/component/teacher/TeacherClasses.jsx
@@ -8,11 +8,11 @@ const TeacherClasses = () => {
   const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
-    actions.getTeacherClasses();
+    actions.GetSchedules()
   }, []); 
-  const filteredClasses = teacherClasses.filter((cls) =>
-    cls.name.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+  // const filteredClasses = teacherClasses.filter((cls) =>
+  //   cls.name.toLowerCase().includes(searchTerm.toLowerCase())
+  // );
 
   const handleSearchChange = (e) => {
     setSearchTerm(e.target.value);
@@ -41,23 +41,19 @@ const TeacherClasses = () => {
             Nombre de la Clase
           </th>
           <th className="tw-px-6 tw-py-3 tw-text-left tw-text-xs tw-font-medium tw-text-gray-500 tw-uppercase tw-tracking-wider">
-            Edad
+            Dia
           </th>
           <th className="tw-px-6 tw-py-3 tw-text-left tw-text-xs tw-font-medium tw-text-gray-500 tw-uppercase tw-tracking-wider">
             Hora
           </th>
-          <th className="tw-px-6 tw-py-3 tw-text-left tw-text-xs tw-font-medium tw-text-gray-500 tw-uppercase tw-tracking-wider">
-            Capacidad
-          </th>
         </tr>
       </thead>
       <tbody className="tw-bg-white tw-divide-y tw-divide-gray-200">
-        {filteredClasses.map((cls) => (
+        {store.schedules.map((cls) => (
           <tr key={cls.id}>
-            <td className="tw-px-6 tw-py-4 tw-whitespace-nowrap">{cls.name}</td>
-            <td className="tw-px-6 tw-py-4 tw-whitespace-nowrap">{cls.age}</td>
-            <td className="tw-px-6 tw-py-4 tw-whitespace-nowrap">{cls.time}</td>
-            <td className="tw-px-6 tw-py-4 tw-whitespace-nowrap">{cls.capacity}</td>
+            <td className="tw-px-6 tw-py-4 tw-whitespace-nowrap">{cls.class}</td>
+            <td className="tw-px-6 tw-py-4 tw-whitespace-nowrap">{cls.dayOfWeek}</td>
+            <td className="tw-px-6 tw-py-4 tw-whitespace-nowrap">{`${cls.startTime} - ${cls.endTime}`}</td>
           </tr>
         ))}
       </tbody>
